de18ba78c0f2dc826d9a809ae1bac306758fd129
Ilya Altukhov <zompin@ya.ru>
2024-07-01 23:35:50 +0300

    ref: Background refactoring

--- background.js
+++ background.js
@@ -3,47 +3,47 @@ import { ACTIONS, MODES } from './background-constants.js'
 
 let mode = MODES.SHADOW
 
-async function main() {
-    browser.tabs.onActivated.addListener(({ tabId }) => {
-        browser.tabs.sendMessage(tabId, JSON.stringify([ACTIONS.SET_MODE, mode]))
-    })
-    browser.runtime.onMessage.addListener(async (params) => {
-        const [key, arg1] = JSON.parse(params)
-        switch (key) {
-            case ACTIONS.TAB_PREV:
-                await Tabs.prevTab()
-                break
-            case ACTIONS.TAB_NEXT:
-                await Tabs.nextTab()
-                break
-            case ACTIONS.TAB_NEW_BACKGROUND:
-                await Tabs.newBackgroundTab(arg1)
-                break
-            case ACTIONS.TAB_MOVE_TO_LEFT:
-                await Tabs.moveCurrentTabToLeft()
-                break
-            case ACTIONS.TAB_MOVE_TO_RIGHT:
-                await Tabs.moveCurrentTabToRight()
-                break
-            case ACTIONS.TAB_DUPLICATE:
-                await Tabs.duplicateTab(arg1)
-                break
-            case ACTIONS.TAB_CLOSE:
-                await Tabs.closeCurrentTab()
-                break
-            case ACTIONS.TAB_RELOAD:
-                await Tabs.reloadCurrentTab()
-                break
-            case ACTIONS.TAB_NEW:
-                await Tabs.newTab()
-                break
-            case ACTIONS.SET_MODE:
-                mode = arg1
-                break
-            case ACTIONS.GET_MODE:
-                return mode
-        }
-    })
+function handleActivate({ tabId }) {
+    browser.tabs.sendMessage(tabId, JSON.stringify([ACTIONS.SET_MODE, mode]))
 }
 
-main().catch(console.log)
+async function handleMessage(params) {
+    const [key, arg1] = JSON.parse(params)
+    switch (key) {
+        case ACTIONS.TAB_PREV:
+            await Tabs.prevTab()
+            break
+        case ACTIONS.TAB_NEXT:
+            await Tabs.nextTab()
+            break
+        case ACTIONS.TAB_NEW_BACKGROUND:
+            await Tabs.newBackgroundTab(arg1)
+            break
+        case ACTIONS.TAB_MOVE_TO_LEFT:
+            await Tabs.moveCurrentTabToLeft()
+            break
+        case ACTIONS.TAB_MOVE_TO_RIGHT:
+            await Tabs.moveCurrentTabToRight()
+            break
+        case ACTIONS.TAB_DUPLICATE:
+            await Tabs.duplicateTab(arg1)
+            break
+        case ACTIONS.TAB_CLOSE:
+            await Tabs.closeCurrentTab()
+            break
+        case ACTIONS.TAB_RELOAD:
+            await Tabs.reloadCurrentTab()
+            break
+        case ACTIONS.TAB_NEW:
+            await Tabs.newTab()
+            break
+        case ACTIONS.SET_MODE:
+            mode = arg1
+            break
+        case ACTIONS.GET_MODE:
+            return mode
+    }
+}
+
+browser.tabs.onActivated.addListener(handleActivate)
+browser.runtime.onMessage.addListener(handleMessage)
